46dd25527fe2517fac25fd0e9f1a09f6e9787a9f
Shani Halali <shanihlli1234@gmail.com>
2025-12-27 10:33:14 +0200

    Initial Commit

---
+++ YogaDemoApp/YogaDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YogaDemoAppApp.swift
+//  YogaDemoApp
+//
+//  Created by Lior.Avraham on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct YogaDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
